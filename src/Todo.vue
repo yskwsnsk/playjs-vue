d517dfb6a3a88c03c74700e6ae0b6653b7ac16ab
play.js <git@gituser.com>
2021-03-31 22:11:53 +0900

    a

---
+++ src/Todo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="todo">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+
+export default{
+  
+}
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import Router from './router'
 
 Vue.config.productionTip = false
 
